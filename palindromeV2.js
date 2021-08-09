b97449293f64b8ca68002c515a97a90ed2f33604
Danielle Evans <iamdanielledeveloper@gmail.com>
2021-08-09 16:03:02 -0400

    JavaScript Algorithm

---
+++ palindromeV2.js
@@ -0,0 +1,20 @@
+function palindrome(str) {
+    // Create empty string
+    let reversed = "";
+    // Loop through string given to function
+    for(let i = 0; i < str.length; i++) {
+        // Add each character of original string
+        // to beginning of new string
+        reversed = str[i] + reversed;
+    }
+    // Compare original string to new string
+    // Return boolean value
+    return str === reversed;
+  }
+  
+  palindrome('kayak');
+  palindrome('apple');
+  palindrome('refer');
+  palindrome('123 456');
+  palindrome('racecar!');
+  palindrome('level');
